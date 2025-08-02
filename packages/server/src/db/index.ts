b6ec7370fd8d0651a9126e18a00a49271412e650
dylan <dylanbradshaw107@hotmail.com>
2025-08-02 23:33:35 +1200

    tweak drizzle

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -3,7 +3,11 @@ import * as schema from './schema';
 import {Pool} from "pg";
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST!,
+    port: parseInt(process.env.DATABASE_PORT!),
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    database: process.env.DATABASE_NAME!,
     ssl: {
         rejectUnauthorized: true,
         ca: process.env.DATABASE_CA,
