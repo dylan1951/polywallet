9e417af44c13ae3e085056e0b4ba2e991f030e19
dylan <dylanbradshaw107@hotmail.com>
2025-08-02 23:10:34 +1200

    tweak drizzle

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -1,8 +1,20 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
+import {Pool} from "pg";
+
+console.log('process.env.DATABASE_CA_CERT', process.env.DATABASE_CA_CERT);
+console.log('process.env.DATABASE_CA', process.env.DATABASE_CA);
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: process.env.DATABASE_CA_CERT,
+    },
+});
 
 export const db = drizzle({
-    connection: process.env.DATABASE_URL!,
+    client: pool,
     schema,
     casing: 'snake_case'
 });
